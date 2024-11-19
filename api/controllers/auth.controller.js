a9260eb8b5320e4410f1e5135507a3f11dce8dd6
Alexis033 <10guevarafm1102@gmail.com>
2024-11-19 18:34:36 -0500

    Eliminar .package-lock.json de node_modules

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,28 @@
 import bcrypt from "bcrypt"
-export const register= async(req, res)=>{
-    const {name, email, password} = req.body
+import prisma from "../lib/prisma.js"
 
-    const hashedPassword = await bcrypt.hash(password, 12)
-    console.log(hashedPassword)
-    // res.send(req.body)
+export const register= async(req, res)=>{
+    const {username, email, password} = req.body
+
+    try{   
+        const hashedPassword = await bcrypt.hash(password, 12)
+        console.log(hashedPassword)
+        
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            },
+        })
+        console.log(newUser)
+
+        res.status(201).json({message: "User created"})
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message: "Failed to create user"})
+    }
+        
 }
 export const login= (req, res)=>{
     // db operations
