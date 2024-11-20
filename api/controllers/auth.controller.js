67fc523e948d8e7502f713324cfda4c502970739
Alexis033 <10guevarafm1102@gmail.com>
2024-11-19 19:15:28 -0500

    add login method

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import prisma from "../lib/prisma.js"
 
 export const register= async(req, res)=>{
@@ -24,8 +25,39 @@ export const register= async(req, res)=>{
     }
         
 }
-export const login= (req, res)=>{
-    // db operations
+export const login= async(req, res)=>{
+    const {username, password} = req.body
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        })
+        if(!user){
+            return res.status(401).json({message: "Invalid credentials"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect){
+            return res.status(401).json({message: "Invalid credentials"})
+        }
+        // traditional way of setting cookies:
+        // res.setHeader("Set-Cookie", "test=" + "myValue").json({message: "User logged in"})
+        const age= 1000*60*60*24*7
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET_KEY, {expiresIn: age})
+        // modern way of setting cookies:
+        res.cookie("token", token,{
+            httpOnly: true,
+            // secure: true // only works on https,
+            maxAge: age
+        })
+        .status(200)
+        .json({message: "User logged in"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Failed to login"})
+    }
 
 }
 export const logout= (req, res)=>{
